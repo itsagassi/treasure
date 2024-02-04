a654feb43c9296f494ea8b568c87c3f3f7bb3b15
Andrey Agassi <t-andrey.agassi@traveloka.com>
2024-02-04 11:27:12 +0700

    password gate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,99 @@
+"use client"
 import Image from "next/image";
 import photo from "../../public/HaiRosie.png"
+import Flashback from "@/components/flashback";
+import React, { useState } from "react"
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    password: ""
+  });
+
+  const rightPass = "this love"
+  const [isBeMine, SetIsBeMine] = useState(false);
+  const targetDate = new Date(2024, 1, 5);
+
+  // Function to handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.password === rightPass && isToday(targetDate)) {
+      SetIsBeMine(true);
+    } else {
+      SetIsBeMine(false);
+    }
+  };
+
+  const isToday = (target) => {
+    const today = new Date();
+    return (
+      target.getDate() === today.getDate() &&
+      target.getMonth() === today.getMonth() &&
+      target.getFullYear() === today.getFullYear()
+    );
+  };
+
   return (
     <main className=" bg-white">
-      <div className=" bg-white w-screen h-screen grid justify-center content-center">
-        <div>
-          <Image
-            src={photo}
-            width={118}
-            height={33}
-            alt="nothing"
-          />
-        </div>
-      </div>
-      <div className=" h-screen bg-gradient-to-b from-white to-black">
-
-      </div>
-      <div className=" h-screen bg-black">
-
-      </div>
-
+      {!isBeMine && (
+        <>
+          <div className=" bg-white h-96 w-screen grid justify-center content-center">
+            <form onSubmit={handleSubmit}>
+              <label className=" grid justify-center content-center">
+                <p className=" flex justify-center p-2">enter the keyword</p>
+                <input
+                  className=" pl-2 font-white rounded-lg border border-black"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </label>
+              <br />
+            </form>
+          </div>
+        </>
+      )}
+      {isBeMine && (
+        <>
+          <div className=" bg-white w-screen h-screen grid justify-center content-center">
+            <div>
+              <Image
+                src={photo}
+                width={118}
+                height={33}
+                alt="nothing"
+              />
+            </div>
+          </div>
+          <div className=" h-screen bg-white"></div>
+          <div className=" h-screen bg-white"></div>
+          <div className=" h-screen bg-gradient-to-b from-white to-black grid justify-center content-center">
+            <p className=" transform rotate-90 text-black">
+              NGEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEENG..........................................
+            </p>
+          </div>
+          <div className=" h-screen bg-black"></div>
+          <div className=" h-screen w-screen bg-black grid justify-center content-center">
+            <h3 className=" text-white">
+              before we go forward...
+            </h3>
+          </div>
+          <div className=" text-white h-screen bg-black grid justify-center content-center">
+            
+          </div>
+          <Flashback />
+        </>
+      )}
     </main>
   );
 }
---
+++ src/components/flashback.tsx
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Flashback() {
+    const sectionRef = useRef(null);
+    const triggerRef = useRef(null);
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useEffect(() => {
+        const pin = gsap.to(sectionRef.current, {
+            translateX: "-500vw",
+            ease: "none",
+            duration: 1,
+            scrollTrigger: {
+                trigger: triggerRef.current,
+                start: "top top",
+                end: "+=2000",
+                scrub: 0.6,
+                pin: true
+            }
+        })
+
+        return () => {
+            pin.kill()
+        }
+    }, [])
+
+    return (
+        <section className=" overflow-hidden">
+            <div ref={triggerRef}>
+                <div className=" h-screen w-[600vw] flex flex-row relative" ref={sectionRef}>
+                    <div className=" h-screen w-screen bg-black grid justify-center content-center text-white ">
+                        <p>let's</p>
+                        <p className=" font-serif font-bold font">REWIND...</p>
+                    </div>
+                    <div className=" h-screen w-[400vw] bg-gradient-to-r from-black to-white"></div>
+                    <div className=" h-screen w-screen bg-white"></div>
+                </div>
+            </div>
+        </section>
+    )
+}
